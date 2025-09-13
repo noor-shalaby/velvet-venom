d8f114d7711d2d674af1eff60806ee9eff97c741
Noor Shalaby <ono.shalaby@gmail.com>
2025-09-13 14:28:37 +0300

    coded the Slider to be manipulated with the keyboard

---
+++ scripts/ui/slider.gd
@@ -0,0 +1,15 @@
+extends HSlider
+
+
+@export var drag_speed: float = 0.05
+
+
+func _process(delta: float) -> void:
+	if not has_focus():
+		return
+	
+	var input_dir_x: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if input_dir_x == 0.0:
+		return
+	
+	value += drag_speed * input_dir_x
