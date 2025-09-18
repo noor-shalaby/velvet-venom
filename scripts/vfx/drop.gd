d4d7ba574c1159dfefd95bad241a6b3a5f5074c8
Noor Shalaby <ono.shalaby@gmail.com>
2025-09-18 18:26:36 +0300

    created Drop as a superclass of the DroppedWeapon to use it in later dropped objects like dropped limbs

---
+++ scripts/vfx/drop.gd
@@ -0,0 +1,16 @@
+class_name Drop
+extends Node2D
+
+
+@export var animation_duration: float = 0.5
+
+@export var drop_range_min: float = 64.0
+@export var drop_range_max: float = 128.0
+
+
+func _ready() -> void:
+	var random_dir: Vector2 = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
+	var tween: Tween = create_tween().set_parallel(true).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(self, "global_position", global_position + randf_range(drop_range_min, drop_range_max) * random_dir, animation_duration)
+	tween.tween_property(self, "global_rotation_degrees", global_rotation_degrees + randf(), animation_duration)
+	tween.tween_property(self, "global_scale", global_scale * 0.9, animation_duration)
